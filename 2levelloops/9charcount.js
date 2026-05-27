45776e96f56b77e0aa6b3c42d7cb48755b787f69
satyaprasanthuppalapu <satyaprasanthuppalapu@email.com>
2026-05-28 00:01:30 +0530

    level2

---
+++ 2levelloops/9charcount.js
@@ -0,0 +1,5 @@
+let str = "hi world";
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]+" is at index "+i);
+}
+console.log("Total characters in string: " + (str.length-1));
--- 2loops/9.js
+++
@@ -1,6 +0,0 @@
-let nums = [10, 20, 30, 40, 50];
-for (let i = 0; i < nums.length; i++) {
-    let coun = 0;
-    coun++;
-}
-console.log(coun);
