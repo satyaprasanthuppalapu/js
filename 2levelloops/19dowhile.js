30e133839e63c59a53e255b05acbcb81bfcbe354
satyaprasanthuppalapu <satyaprasanthuppalapu@email.com>
2026-05-30 16:34:58 +0530

    commit -m "Added do-while loop example and a simple function example"

---
+++ 2levelloops/19dowhile.js
@@ -0,0 +1,6 @@
+//do while loop
+let count = 1;
+do {
+  console.log("Count is", count);
+  count++;
+} while (count <= 5);
---
+++ level3functions/1fun.js
@@ -0,0 +1,9 @@
+// 2fun.js
+// Add your JavaScript functions below.
+
+function exampleFunction() {
+    console.log('Hello from 2fun.js');
+}       
+
+// Example usage:
+exampleFunction();
